49be41e304dc30cd0164cf9b3a90cc1965de5719
atygaev.mi <atygaev.mi@gmail.com>
2022-07-20 16:34:58 +0700

    Initial

---
+++ src/main/java/com/example/dbswticher/DbSwticherApplication.java
@@ -0,0 +1,12 @@
+package com.example.dbswticher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSwticherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbSwticherApplication.class, args);
+    }
+}
