86b61bde3187123c2b0681df3d0c63a131b614d7
atygaev.mi <atygaev.mi@gmail.com>
2022-07-20 23:28:39 +0700

    #1 Fix review issues: move db info output to separated class

---
+++ src/main/java/com/example/dbswticher/DbDriverInfoPrinter.java
@@ -0,0 +1,37 @@
+package com.example.dbswticher;
+
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class DbDriverInfoPrinter {
+
+    private final Environment environment;
+
+    public DbDriverInfoPrinter(Environment environment) {
+        this.environment = environment;
+    }
+
+    @EventListener
+    public void handle(ApplicationStartedEvent event) {
+        System.out.printf("Active profile: %s%n", Arrays.toString(environment.getActiveProfiles()));
+
+        String[] driverClassNames = {
+                "org.h2.Driver",
+                "org.postgresql.Driver"
+        };
+
+        for (String driverClassName : driverClassNames) {
+            try {
+                Class.forName(driverClassName);
+                System.out.printf("Driver [%s] is loaded%n", driverClassName);
+            } catch (ClassNotFoundException e) {
+                System.out.printf("Driver [%s] is missing%n", driverClassName);
+            }
+        }
+    }
+}
